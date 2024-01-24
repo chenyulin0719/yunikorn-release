0c6eb2d76221301d26ea9c091a3145b8457d59ac
Yu-Lin Chen <kh87313@gmail.com>
2024-01-24 06:34:38 +0000

    Set HOMEDIR

--- build-web-and-load.sh
+++ build-web-and-load.sh
@@ -2,5 +2,5 @@
 
 
 REGISTRY=local VERSION=latest make clean image
-kind load docker-image local/yunikorn:web-amd64-latest  --name yunikorn
+TMPDIR=$HOME kind load docker-image local/yunikorn:web-amd64-latest  --name yunikorn
 kubectl delete pods -l component=yunikorn-scheduler -n yunikorn
